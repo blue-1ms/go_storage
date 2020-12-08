0d0ca2f392b90a6a7a2033f15da8927b7b3fe090
Oscar <tonyzhao1314@gmail.com>
2020-12-09 03:36:11 +1100

    Add files via upload

---
+++ ascii_art.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main () {
+  fmt.Println("  __      _")
+  fmt.Println("  __      _")
+  fmt.Println("  __      _")
+  fmt.Println("  __      _")
+  fmt.Println("  __      _")
+  fmt.Println("this is printed on" time.Now())
+}
